d86ed6ff4ba38aa6d5871b7e21476ccd2ac8b16e
oleksandr-gluhov <84685240+oleksandr-gluhov@users.noreply.github.com>
2023-09-17 17:00:16 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -22,10 +22,10 @@ fetch('https://weatherbit-v1-mashape.p.rapidapi.com/forecast/daily?lat=49.2&lon=
 			max[i].innerHTML = data[i].max_temp
 			min[i].innerHTML = data[i].min_temp
 			if (data[i].max_temp > 15 ) {
-				vusnovok[i].innerHTML = "Я і Вася в валяються в фосі"
+				vusnovok[i].innerHTML = "Гарна погода"
 			  }
 			else{
-				vusnovok[i].innerHTML = 'хуй а не пянка'
+				vusnovok[i].innerHTML = 'Погана погода'
 			}
 		  }
 	
